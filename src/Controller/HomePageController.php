ad930261e279d32af021432e4e446ec992e0b081
Igor Petrov <petrov120482@gmail.com>
2020-06-19 13:21:14 +0300

    Регистрация , авторизация, востановление пароля, AdminLTE, загрузка изображений , главная страница.

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\HomePageContentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomePageController extends AbstractController
@@ -10,10 +12,10 @@ class HomePageController extends AbstractController
     /**
      * @Route("/", name="home_page")
      */
-    public function index()
+    public function index(HomePageContentRepository $homePageContentRepository): Response
     {
         return $this->render('home_page/index.html.twig', [
-
+            'home_page_contents' => $homePageContentRepository->findAll(),
         ]);
     }
 }
