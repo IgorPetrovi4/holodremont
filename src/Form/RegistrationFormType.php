cd87fbc3ed7542a58db4ba8c0186398fd4ec87e2
Igor Petrov <petrov120482@gmail.com>
2020-06-19 14:54:01 +0300

    иправлен регистрация шаблон

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,26 +18,30 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class,[
+                'label' => 'Электронная почта',
+            ])
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'Соглашаюсь с условиями',
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Вы должны согласиться с нашими условиями.',
                     ]),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
+                'label' => 'Пароль',
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Пожалуйста, введите пароль',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Ваш пароль должен содержать не менее {{limit}} символов',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
